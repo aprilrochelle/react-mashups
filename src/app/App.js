969c86d4b861ddddd5f39edea7afe9f9a6980bd0
April Nichols <nicholsar@gmail.com>
2018-06-27 19:38:40 -0500

    form component setup

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Animals from '../components/Animals/Animals';
+import Form from '../components/Form/Form';
 import './App.css';
 
 class App extends React.Component {
   render () {
     return (
       <div className="App">
-        <Animals />
+        <div className="col-md-8">
+          <Animals />
+        </div>
+        <div className="col-md-4">
+          <Form />
+        </div>
       </div>
     );
   }
